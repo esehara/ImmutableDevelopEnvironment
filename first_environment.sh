f9a044c7575b3a38e7c51be285c7a615ebff7cb5
esehara shigeo <esehara@gmail.com>
2014-11-07 19:42:06 +0900

    fix: Install Vim and Set git main editor

--- first_environment.sh
+++ first_environment.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+## Main Editor
+sudo apt-get install -y vim
+
 ## Python Environment
-sudo pip install paramiko PyYAML Jinja2 httplib2
+sudo pip install -y paramiko PyYAML Jinja2 httplib2
 
 ## Git Configure
 git config --global user.email "esehara@gmail.com"
 git config --global user.name "esehara shigeo"
+git config --global core.editor 'vim -c "set fenc=utf-8"'
 
 ## Ansile Install
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repostiory ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install -y ansible
