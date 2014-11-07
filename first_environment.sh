182b847bf73ce02e35cfcf2a7dfa5ad8f5d8e03b
esehara shigeo <esehara@gmail.com>
2014-11-07 19:16:53 +0900

    first commit --- first environment

---
+++ first_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Python Environment
+sudo pip install paramiko PyYAML Jinja2 httplib2
+
+## Git Configure
+git config --global user.email "esehara@gmail.com"
+git config --global user.name "esehara shigeo"
