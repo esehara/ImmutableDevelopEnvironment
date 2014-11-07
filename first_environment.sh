0de45d0e8eba5e66e97f3a4986af9d09094aba66
esehara shigeo <esehara@gmail.com>
2014-11-07 19:30:20 +0900

    Install Ansible

--- first_environment.sh
+++ first_environment.sh
@@ -6,3 +6,9 @@ sudo pip install paramiko PyYAML Jinja2 httplib2
 ## Git Configure
 git config --global user.email "esehara@gmail.com"
 git config --global user.name "esehara shigeo"
+
+## Ansile Install
+sudo apt-get install software-properties-common
+sudo apt-add-repostiory ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
