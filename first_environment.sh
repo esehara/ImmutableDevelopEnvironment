5820621256d0e12d78081f693868eb4b2af7e9eb
esehara shigeo <esehara@gmail.com>
2014-11-08 14:48:18 +0900

    add: mkdir usually directory for development

--- first_environment.sh
+++ first_environment.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# First Development Envionment
+#
+# Description:
+#  Happy Hacking Development short time ;)
+#
+# Usage:
+#   wget -Oq- https://raw.githubusercontent.com/esehara/ImmutableDevelopEnvironment/master/first_environment.sh | sh
+# 
 
 ## Main Editor
 sudo apt-get install -y vim
@@ -16,3 +25,13 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repostiory ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
+
+## If clean environment anyway, mkdir 
+if [ ! -d $HOME/Code ];then;
+  mkdir $HOME/Code
+fi
+
+if [ ! -d $HOME/base ];then;
+  mkdir $HOME/base
+  cd $HOME/base;git clone git@github.com:esehara/ImmutableDevelopEnvironment.git
+fi
