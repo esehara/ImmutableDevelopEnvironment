fb0a9328f36c039710455fd9efa37ec117d4b674
esehara shigeo <esehara@gmail.com>
2014-11-08 17:41:20 +0900

    add: templates emacs24 init.el

---
+++ roles/templates/emacs24/init.el
@@ -0,0 +1,7 @@
+;; Package Mangement 
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+  '("marmalede" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
