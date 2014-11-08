20f04263303df5c92e63f934b8d231d895ac7d4a
esehara shigeo <esehara@gmail.com>
2014-11-08 18:39:58 +0900

    add: install emacs packages1

--- roles/templates/emacs24/auto-install.el
+++ roles/templates/emacs24/auto-install.el
@@ -1,8 +1,21 @@
 ;; auto installer
 
 (require 'cl)
-(defvar installing-package-list
-   '(evil))
+(defvar installing-package-list 
+  '(
+    auto-complete
+    autopair
+    evil
+    magit
+    org
+    solarized-theme
+    markdown-mode
+    yaml-mode
+    windmove))
+
+(defvar default-set-mode
+  '(evil-mode))
+
 (let
   ((not-installed (loop for x in installing-package-list
     when (not (package-installed-p x)) collect x)))
@@ -10,5 +23,9 @@
     (package-refresh-contents)
     (dolist (pkg not-installed) (package-install pkg))))
 
-(require 'evil)
-(evil-mode t)
+(mapc 
+  (lambda (x) (require (quote x)))
+  installing-package-list)
+
+
+(mapc (lambda (x) (x t)) default-set-mode)
