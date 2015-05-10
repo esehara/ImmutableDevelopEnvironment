bf5658e286a2ce74691050e2ec9061d64eff8621
esehara shigeo <esehara@gmail.com>
2015-05-10 10:30:12 +0900

    fix: `if ~ then` syntax not need semi-collon

--- first_environment.sh
+++ first_environment.sh
@@ -31,11 +31,11 @@ sudo apt-get update
 sudo apt-get install -y ansible
 
 ## If clean environment anyway, mkdir 
-if [ ! -d $HOME/Code ];then;
+if [ ! -d $HOME/Code ];then
   mkdir $HOME/Code
 fi
 
-if [ ! -d $HOME/base ];then;
+if [ ! -d $HOME/base ];then
   mkdir $HOME/base
   cd $HOME/base;git clone git@github.com:esehara/ImmutableDevelopEnvironment.git
 fi
